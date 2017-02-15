0363d12bdbe34541f879718a1e91f8967fd413f7
tyler <tyler@jackoldmacair.local>
2017-02-14 21:05:02 -0800

    add server

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'client')));
+app.use('/scripts', express.static(path.join(__dirname, 'node_modules')));
+
+var server = app.listen(3000);
+console.log('Listening on port 3000');
