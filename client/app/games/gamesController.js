b015c14e965f03da842e317e4ab5cb6751fe19d9
tyler <tyler@jackoldmacair.local>
2017-02-22 12:21:58 -0800

    Begin formatting for games library

--- client/app/games/gamesController.js
+++ client/app/games/gamesController.js
@@ -1,4 +1,26 @@
 angular.module('gameSuite')
 .controller('gamesController', function($scope) {
-    $scope.text = "This is where the games will be!";
+    $scope.text = 'This is where the games will be!';
+    $scope.games = [
+      {
+        title: 'rock-paper-scissors',
+        description: 'rock-paper-scissors game description'
+      },
+      {
+        title: 'scrabble',
+        description: 'scrabble game desciption'
+      },
+      {
+        title: 'keno',
+        description: 'keno game description'
+      },
+      {
+        title: 'poker',
+        description: 'poker game description'
+      },
+      {
+        title: 'memory',
+        description: 'memory game description'
+      }
+    ];
 });
