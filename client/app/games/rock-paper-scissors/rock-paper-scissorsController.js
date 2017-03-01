f95fd7665cea3b5647c05d81294f04c924f34549
tyler <tyler@jackoldmacair.local>
2017-02-28 21:07:50 -0800

    Add streak sytem to RPS game

--- client/app/games/rock-paper-scissors/rock-paper-scissorsController.js
+++ client/app/games/rock-paper-scissors/rock-paper-scissorsController.js
@@ -1,28 +1,46 @@
 angular.module('gameSuite')
 .controller('rock-paper-scissorsController', function($scope, $timeout) {
-  $scope.text = "This is where the rock-paper-scissors game will be!";
+
   $scope.choices = ['Rock', 'Paper', 'Scissors'];
+  $scope.currentStreak = 0;
+  $scope.highestStreak = 0;
+
   $scope.choosePlay = function(color, piece) {
     $scope.play = null;
     $scope.enemyPlay = null;
     $scope.play = color + piece;
-    $scope.chooseEnemyPlay(color);
+    $scope.chooseEnemyPlay(color, piece);
   }
-  $scope.chooseEnemyPlay = function(color) {
+  $scope.chooseEnemyPlay = function(color, userPiece) {
+    var enemyPiece = $scope.choices[$scope.getRandomInit(0, 3)];
     if (color === 'red') {
       color = 'blue';
     } else if (color === 'blue') {
       color = 'red';
     }
-    var choice = $scope.choices[$scope.getRandomInit(0, 3)];
     $timeout(function() {
-    $scope.enemyPlay = color + choice;
+      $scope.enemyPlay = color + enemyPiece;
+      $scope.updateScore(userPiece, enemyPiece);
     }, 100)
   }
   $scope.getRandomInit = function(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
+  }
+  $scope.updateScore = function(userPiece, enemyPiece) {
+    if (userPiece === 'Rock' && enemyPiece === 'Scissors' ||
+      userPiece === 'Paper' && enemyPiece === 'Rock' ||
+      userPiece === 'Scissors' && enemyPiece === 'Paper') {
+      $scope.currentStreak++;
+    } else if (enemyPiece === 'Rock' && userPiece === 'Scissors' ||
+      enemyPiece === 'Paper' && userPiece === 'Rock' ||
+      enemyPiece === 'Scissors' && userPiece === 'Paper') {
+      $scope.currentStreak = 0;
+    }
+    if ($scope.currentStreak > $scope.highestStreak) {
+     $scope.highestStreak = $scope.currentStreak
+    }
   }
 });
 
