de8239890abf884989de41d006a7cf2c9ebc68a6
tyler <tyler@jackoldmacair.local>
2017-02-28 21:36:01 -0800

    Add routes to game objects

--- client/app/games/gamesPage/gamesPageController.js
+++ client/app/games/gamesPage/gamesPageController.js
@@ -12,26 +12,31 @@ angular.module('gameSuite')
     $scope.games = [
       {
         title: 'rock-paper-scissors',
-        description: 'rock-paper-scissors game description'
+        description: 'rock-paper-scissors game description',
+        route: 'rock-paper-scissors'
       },
       {
         title: 'scrabble',
-        description: 'scrabble game desciption'
+        description: 'scrabble game desciption',
+        route: 'scrabble'
+      },
+      {
+        title: 'Typing Tank',
+        description: 'Typing Tank game description',
+        route: 'typing-tank'
       },
       {
         title: 'keno',
-        description: 'keno game description'
-      },
-      {
-        title: 'poker',
-        description: 'poker game description'
+        description: 'keno game description',
+        route: 'keno'
       },
       {
         title: 'memory',
-        description: 'memory game description'
+        description: 'memory game description',
+        route: 'memory'
       }
     ];
-    $scope.changeLocation = function(game) {
-      $location.path('/games/' + game);
+    $scope.changeLocation = function(gameRoute) {
+      $location.path('/games/' + gameRoute);
     };
 });
